2e3af4c7f8f8043981255282ef7c80b516de7ec0
Fred Vassard <fredvassard@sparkcentral.com>
2017-02-17 13:31:01 -0800

    debugging mesages

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Running init...."
+
 if [ $# -eq 0 ]
 then
     echo "You must specify the name of the project as an argument!"
@@ -6,6 +8,7 @@ then
 fi
 
 # Replacing every occurence of Sample with the project name.
+echo "Finding all the occurences of 'sample' and replacing them with $1..."
 for f in `rg myproj -l`; do 
     sed -i.bak "s/myproj/$1/" $f
     find ./ -name '*.bak' -delete
