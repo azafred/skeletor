b45d2b29fcf386ea63df150e38a4f5aae4e9dca1
Fred Vassard <fred@studyblue.com>
2018-01-30 10:31:29 -0800

    forgot one

--- release.sh
+++ release.sh
@@ -18,7 +18,7 @@ VNUM3=$((VNUM3+1))
 NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
 
 echo "Updating $VERSION to $NEW_TAG"
-echo "__version__ = '$NEW_TAG'" > loony/version.py
+echo "__version__ = '$NEW_TAG'" > sample/version.py
 git commit -am "Updating to Version $NEW_TAG"
 #get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
@@ -28,20 +28,20 @@ NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 if [ -z "$NEEDS_TAG" ]; then
     echo "Tagged with $NEW_TAG"
     git tag $NEW_TAG
-    pyinstaller loony/main.py --onefile --clean -p ./loony -n loony_${NEW_TAG}_macos --hidden-import=Queue
-    docker run -v "$(pwd):/src/" cdrx/pyinstaller-linux:python2 "pyinstaller --onefile --clean -p ./loony -n loony_${NEW_TAG}_linux --hidden-import=Queue loony/main.py"
-    rm dist/loony
-    ln -s loony_${NEW_TAG}_macos dist/loony
+    pyinstaller sample/main.py --onefile --clean -p ./sample -n sample_${NEW_TAG}_macos --hidden-import=Queue
+    docker run -v "$(pwd):/src/" cdrx/pyinstaller-linux:python2 "pyinstaller --onefile --clean -p ./sample -n sample_${NEW_TAG}_linux --hidden-import=Queue sample/main.py"
+    rm dist/sample
+    ln -s sample_${NEW_TAG}_macos dist/sample
     echo "${NEW_TAG}" > dist/latest_version.txt
     aws s3 cp ./dist/latest_version.txt s3://studyblue-binaries/latest_version.txt
-    aws s3 cp ./dist/loony_${NEW_TAG}_macos s3://studyblue-binaries/archives/
-    aws s3 cp ./dist/loony_${NEW_TAG}_macos s3://studyblue-binaries/loony_macos_latest
-    aws s3 cp ./dist/loony_${NEW_TAG}_linux s3://studyblue-binaries/archives/
-    aws s3 cp ./dist/loony_${NEW_TAG}_linux s3://studyblue-binaries/loony_linux_latest
+    aws s3 cp ./dist/sample_${NEW_TAG}_macos s3://studyblue-binaries/archives/
+    aws s3 cp ./dist/sample_${NEW_TAG}_macos s3://studyblue-binaries/sample_macos_latest
+    aws s3 cp ./dist/sample_${NEW_TAG}_linux s3://studyblue-binaries/archives/
+    aws s3 cp ./dist/sample_${NEW_TAG}_linux s3://studyblue-binaries/sample_linux_latest
     python setup.py sdist
-    twine upload ./dist/loony-${NEW_TAG}.tar.gz
-    #git add dist/loony_${NEW_TAG}_macos
-    #git commit -am "Adding new binary: loony_${NEW_TAG}_macos"
+    twine upload ./dist/sample-${NEW_TAG}.tar.gz
+    #git add dist/sample_${NEW_TAG}_macos
+    #git commit -am "Adding new binary: sample_${NEW_TAG}_macos"
     git push --tags
     git push
 else
