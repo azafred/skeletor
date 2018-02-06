4329eaf425522cffefb257710639fbb633936cec
Fred Vassard <fred@studyblue.com>
2018-02-06 13:48:58 -0800

    cleanup

--- sample/config.py
+++
@@ -1,2 +0,0 @@
-debug=False
-verbose=False
--- sample/main.py
+++ sample/main.py
@@ -2,9 +2,9 @@
 
 import argparse
 import logging
-import config
-from version import __version__
-
+import mod_config
+import mod_version
+from colorama import Fore, Style
 
 def main():
     """ Main Loop """
@@ -20,27 +20,37 @@ def main():
         help='Show version information', dest='version')
 
     args = parser.parse_args()
-    if args.verbose:
-        print("verbose")
-        logging.basicConfig(level=logging.INFO)
-        logging.getLogger().setLevel(logging.INFO)
-        requests_log = logging.getLogger("requests.packages.urllib3")
-        requests_log.setLevel(logging.INFO)
-        requests_log.propagate = True
+    
     if args.debug:
-        logging.basicConfig(level=logging.DEBUG)
+        logformat =  Fore.RED + '%(asctime)s %(module)s-%(funcName)s %(message)s' + Style.RESET_ALL
+        logging.basicConfig(level=logging.DEBUG, format=logformat, datefmt='%m/%d/%Y %H:%M:%S ')
         logging.getLogger().setLevel(logging.DEBUG)
         requests_log = logging.getLogger("requests.packages.urllib3")
         requests_log.setLevel(logging.DEBUG)
         requests_log.propagate = True
+        deblog = logging.getLogger(__name__)
+        deblog.setLevel(logging.DEBUG)
+        deblog.propagate = True
+        deblog.debug("[ DEBUG Mode Enabled! ] ")
+    if args.verbose:
+        logformat =  Fore.BLUE + '%(asctime)s %(module)s-%(funcName)s %(message)s' + Style.RESET_ALL
+        logging.basicConfig(level=logging.INFO, format=logformat, datefmt='%m/%d/%Y %H:%M:%S ')
+        logging.getLogger().setLevel(logging.INFO)
+        requests_log = logging.getLogger("requests.packages.urllib3")
+        requests_log.setLevel(logging.INFO)
+        requests_log.propagate = True
+        verlog = logging.getLogger(__name__)
+        verlog.setLevel(logging.INFO)
+        verlog.propagate = True
+        verlog.info("[ Verbose Mode Enabled! ] ")
     if args.version:
         show_version()
-    config.verbose = args.verbose
-    config.debug = args.debug
+    mod_config.verbose = args.verbose
+    mod_config.debug = args.debug
 
 
 def show_version():
-    print("sample version {} ".format(__version__))
+    print("sample version {} ".format(mod_version.__version__))
 
 
 if __name__ == '__main__':
